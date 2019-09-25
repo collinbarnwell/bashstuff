3373413f30d5d92e3f54eca0d7bc7c644566568a
Collin Barnwell <collinbarnwell@google.com>
2019-09-25 16:27:49 -0700

    inotify to update emacs tags

--- bashstuff.bash
+++ bashstuff.bash
@@ -10,3 +10,17 @@ parse_git_branch() {
 }
 
 export PS1="\[\033[32m\]\w\[\033[33m\]\$(parse_git_branch)\[\033[00m\] $ "
+
+CODE_DIR="$HOME/c"
+TAG_FILE="$CODE_DIR/TAGS"
+INOTIFY_CMD="inotifywait --quiet --exclude=TAGS -r -e close_write,moved_to,create -m $CODE_DIR"
+
+if pgrep --full "${INOTIFY_CMD}"  > /dev/null
+then
+true
+else
+$INOTIFY_CMD |
+while read -r directory events filename; do
+  find $CODE_DIR -name '*.c' -o -name '*.h' -exec etags --output=$TAG_FILE -a {} \;
+done &
+fi
