e28adc1d88c393345ef094f0f65bf4e2732c3ee9
Collin Barnwell <collinbarnwell@google.com>
2021-03-19 15:53:21 -0700

    make zsh compat

--- bashstuff.sh
+++ bashstuff.sh
@@ -5,22 +5,21 @@ export VISUAL='emacs'
 
 export PATH=$PATH:/home/collinbarnwell/.local/bin
 
-parse_git_branch() {
-     git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
-}
+# bash only:
+# parse_git_branch() {
+     # git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
+# }
 
-export PS1="\[\033[32m\]\w\[\033[33m\]\$(parse_git_branch)\[\033[00m\] $ "
+# export PS1="\[\033[32m\]\w\[\033[33m\]\$(parse_git_branch)\[\033[00m\] $ "
 
 
 # for dir in $HOME/c/*/     # list directories in the form "/fullpath/dirname/"
 # do
 
-CODE_DIR=${dir%*/}      # remove the trailing "/"
-TAG_FILE="$CODE_DIR/TAGS"
-
-alias makectags='ctags -R common/ project/'
-
-alias makenote='export FILE=~/notes/note-$(date "+%F").txt ; touch "$FILE" ; emacs "$FILE" & '
+# CODE_DIR=${dir%*/}      # remove the trailing "/"
+# TAG_FILE="$CODE_DIR/TAGS"
+# alias makectags='ctags -R common/ project/'
+# alias makenote='export FILE=~/notes/note-$(date "+%F").txt ; touch "$FILE" ; emacs "$FILE" & '
 
 # INOTIFY_CMD="inotifywait --quiet --exclude=TAGS -r -e close_write,moved_to,create -m $CODE_DIR"
 
