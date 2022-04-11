9dc75a99d400f782f1f7e0c784ea8168def3d331
Collin Barnwell <barnwell.collin@gmail.com>
2022-04-11 12:00:57 -0700

    Updates

--- bashstuff.sh
+++ bashstuff.sh
@@ -1,49 +1,15 @@
 alias gclang-format="git clang-format --style=file"
 
-export EDITOR='emacs'
+e() { emacsclient -c "$@" & }
+export EDITOR='emacs -nw'
 export VISUAL='emacs'
 
-export PATH=$PATH:/home/collinbarnwell/.local/bin
-
-# bash only:
-# parse_git_branch() {
-     # git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
-# }
-
-# export PS1="\[\033[32m\]\w\[\033[33m\]\$(parse_git_branch)\[\033[00m\] $ "
-
-
-# for dir in $HOME/c/*/     # list directories in the form "/fullpath/dirname/"
-# do
-
-# CODE_DIR=${dir%*/}      # remove the trailing "/"
-# TAG_FILE="$CODE_DIR/TAGS"
-# alias makectags='ctags -R common/ project/'
-# alias makenote='export FILE=~/notes/note-$(date "+%F").txt ; touch "$FILE" ; emacs "$FILE" & '
-
-# INOTIFY_CMD="inotifywait --quiet --exclude=TAGS -r -e close_write,moved_to,create -m $CODE_DIR"
-
-# Update  TAGS file now.
-# find $CODE_DIR -regex '.*\.cc$' -o -regex '.*\.c$' -o -regex '.*\.h$' -exec etags --output=$TAG_FILE {} \; &
-
-# if pgrep --full "${INOTIFY_CMD}"  > /dev/null  # If inotify proc is not running, start it so that TAGS file updates.
-# then
-# true
-# else
-# $INOTIFY_CMD |
-# while read -r directory events filename; do
-  # find $CODE_DIR -regex '.*\.cc$' -o -regex '.*\.c$' -o -regex '.*\.h$' -exec etags --output=$TAG_FILE {} \;
-# done &
-# fi
-
-# done
+export PATH=$PATH:$HOME/.local/bin
 
 alias k="kubectl"
-e() { emacs "$@" & }
 
 export ctop=collinbarnwell@beech.c.googlers.com
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init --path)"
 
 alias gsu="git submodule update --init --recursive"
+
+export PATH=$PATH:$HOME/go/bin
